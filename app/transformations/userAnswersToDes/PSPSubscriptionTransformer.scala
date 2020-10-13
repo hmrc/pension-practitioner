07172dd75979112c0ded874bb50a2ffbdc32abbc
Madhura <madhuranakate@gmail.com>
2020-10-13 17:08:02 +0100

    PODS-4768 Updated transformations

--- app/transformations/userAnswersToDes/PSPSubscriptionTransformer.scala
+++ app/transformations/userAnswersToDes/PSPSubscriptionTransformer.scala
@@ -32,22 +32,18 @@ class PSPSubscriptionTransformer extends JsonTransformer {
       transformDeclaration).reduce
 
   private def transformSubscriptionDetails: Reads[JsObject] =
-    (
-      (__ \ 'subscriptionTypeAndPSPIDDetails \ 'subscriptionType).json.put(JsString("Creation")) and
+    (((__ \ 'subscriptionTypeAndPSPIDDetails \ 'subscriptionType).json.copyFrom((__ \ 'subscriptionType).json.pick) orElse
+      (__ \ 'subscriptionTypeAndPSPIDDetails \ 'subscriptionType).json.put(JsString("Creation"))) and
         (__ \ 'subscriptionTypeAndPSPIDDetails \ 'existingPSPID).json.copyFrom(
-          (__ \ 'existingPSP \ 'isExistingPSP).read[Boolean].map{
+          (__ \ 'existingPSP \ 'isExistingPSP).read[Boolean].map {
             case true => JsString("Yes")
             case false => JsString("No")
-          }
-        ) and
-      (
-        (__ \ 'subscriptionTypeAndPSPIDDetails \ 'pspid).json.copyFrom((__ \ 'existingPSP \ 'existingPSPId).json.pick) orElse doNothing
-      )
-    ).reduce
+          }) and
+      ((__ \ 'subscriptionTypeAndPSPIDDetails \ 'pspid).json.copyFrom((__ \ 'existingPSP \ 'existingPSPId).json.pick) orElse doNothing)).reduce
 
   private def transformLegalAndCustomer: Reads[JsObject] =
     ((__ \ 'regime).json.put(JsString("PODP")) and
-      (__ \ 'sapNumber).json.copyFrom((__ \ 'registrationInfo \ 'sapNumber).json.pick) and
+      ((__ \ 'sapNumber).json.copyFrom((__ \ 'registrationInfo \ 'sapNumber).json.pick) orElse doNothing) and
       (__ \ 'legalEntityAndCustomerID \ 'legalStatus).json.copyFrom((__ \ 'registrationInfo \ 'legalStatus).json.pick) and
       (__ \ 'legalEntityAndCustomerID \ 'customerType).json.copyFrom((__ \ 'registrationInfo \ 'customerType).json.pick) and
       ((__ \ 'legalEntityAndCustomerID \ 'idType).json.copyFrom((__ \ 'registrationInfo \ 'idType).json.pick) orElse doNothing) and
@@ -57,9 +53,11 @@ class PSPSubscriptionTransformer extends JsonTransformer {
     (__ \ 'registrationInfo \ 'legalStatus).read[String].flatMap {
       case "Individual" =>
         ((__ \ 'individualDetails \ 'firstName).json.copyFrom((__ \ 'individualDetails \ 'firstName).json.pick) and
-          (__ \ 'individualDetails \ 'lastName).json.copyFrom((__ \ 'individualDetails \ 'lastName).json.pick)).reduce
+          (__ \ 'individualDetails \ 'lastName).json.copyFrom((__ \ 'individualDetails \ 'lastName).json.pick) and
+          ((__ \ 'individualDetails \ 'changeFlag).json.copyFrom((__ \ 'nameChange).json.pick) orElse doNothing)).reduce
       case _ =>
-        (__ \ 'orgOrPartnershipDetails \ 'organisationName).json.copyFrom((__ \ 'name).json.pick)
+        ((__ \ 'orgOrPartnershipDetails \ 'organisationName).json.copyFrom((__ \ 'name).json.pick) and
+          ((__ \ 'orgOrPartnershipDetails \ 'changeFlag).json.copyFrom((__ \ 'nameChange).json.pick) orElse doNothing)).reduce
     }
 
   private def transformAddress: Reads[JsObject] =
@@ -69,8 +67,8 @@ class PSPSubscriptionTransformer extends JsonTransformer {
       ((__ \ 'correspondenceAddressDetails \ 'addressLine3).json.copyFrom((__ \ 'contactAddress \ 'addressLine3).json.pick) orElse doNothing) and
       ((__ \ 'correspondenceAddressDetails \ 'addressLine4).json.copyFrom((__ \ 'contactAddress \ 'addressLine4).json.pick) orElse doNothing) and
       (__ \ 'correspondenceAddressDetails \ 'countryCode).json.copyFrom((__ \ 'contactAddress \ 'country).json.pick) and
-      ((__ \ 'correspondenceAddressDetails \ 'postalCode).json.copyFrom((__ \ 'contactAddress \ 'postcode).json.pick) orElse doNothing)
-      ).reduce
+      ((__ \ 'correspondenceAddressDetails \ 'postalCode).json.copyFrom((__ \ 'contactAddress \ 'postcode).json.pick) orElse doNothing) and
+      ((__ \ 'correspondenceAddressDetails \ 'changeFlag).json.copyFrom((__ \ 'addressChange).json.pick) orElse doNothing)).reduce
 
   private def nonUKAddress: Reads[JsObject] =
     (__ \ 'contactAddress \ 'country).read[String].flatMap {
