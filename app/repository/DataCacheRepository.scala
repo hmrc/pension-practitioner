16e525b73f3383868d45b097110b63a91a01903e
Pavel Vjalicin <pavelvjalicin@users.noreply.github.com>
2024-12-18 13:09:54 +0000

    PODS-9952: Removed MigrationService from coverage

--- app/repository/DataCacheRepository.scala
+++ app/repository/DataCacheRepository.scala
@@ -38,7 +38,6 @@ import crypto.DataEncryptor
 import org.mongodb.scala.model._
 import play.api.libs.json._
 import play.api.{Configuration, Logging}
-import uk.gov.hmrc.crypto.EncryptedValue
 import uk.gov.hmrc.mongo.MongoComponent
 import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats
 import uk.gov.hmrc.mongo.play.json.{Codecs, PlayMongoRepository}
